27c90856d74b28352b254921baebefd2501426ab
D4RKPN5RK1BK <70687915+D4RKPN5RK1BK@users.noreply.github.com>
2020-09-05 20:21:12 +0500

    ragistry+

---
+++ data/php/reg/except.php
@@ -0,0 +1,78 @@
+<?php
+    /**
+     *
+     */
+    class RegistryExc
+    {
+        private $excCatcher= false;
+
+        public function checkPhone(string $stringToCheck, string $stringToWrite) {
+
+            if (!$this->excCatcher) {
+                if (!preg_match("/^[1-9][0-9]{10}$|^[1-9]\s[0-9]{3}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}$|^[1-9]-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$/", $stringToCheck)) {
+                    echo "<div class='error'>".$stringToWrite."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function checkEmail(string $stringToCheck, string $stringToWrite) {
+
+            if (!$this->excCatcher) {
+                if (!preg_match("/^[a-zA-Z0-9]{5,}@[a-zA-Z]{3,}.[a-zA-Z]{2,}$/", $stringToCheck)) {
+                    echo "<div class='error'>".$stringToWrite."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function checkAge(string $stringToCheck, string $stringToWrite) {
+            if (!$this->excCatcher) {
+                if (!preg_match("/^[0-9]{1,3}$/", $stringToCheck)) {
+                    echo "<div class='error'>".$stringToWrite."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function checkName(string $stringToCheck, string $stringToWrite) {
+            if (!$this->excCatcher) {
+                if (!preg_match("/^\S+/", $stringToCheck)) {
+                    echo "<div class='error'>".$stringToWrite."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function checkPassword(string $firstPassword, string $secondPassword, string $firstString, string $secondString, string $thirtdString) {
+            if (!$this->excCatcher) {
+                if (!preg_match("/^\S{8,}$/", $firstPassword)) {
+                    echo "<div class='error'>".$firstString."</div>";
+                    $this->excCatcher = true;
+                }
+                else if (!preg_match("/^[0-9a-zA-Z?!%-\.\^\+\(\)\&\[\]<>\$\#\@\№\*\'\"\{\}\,\|\/\:\_]+$/", $firstPassword)) {
+                    echo "<div class='error'>".$secondString."</div>";
+                    $this->excCatcher = true;
+                }
+                else if ($firstPassword != $secondPassword) {
+                    echo "<div class='error'>".$thirtdString."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function checkExistence($existance, string $stringToWrite) {
+            if (!$this->excCatcher) {
+                if ($existence) {
+                    echo "<div class='error'>".$stringToWrite."</div>";
+                    $this->excCatcher = true;
+                }
+            }
+        }
+
+        public function getExceptions() {
+            return $this->excCatcher;
+        }
+    }
+
+?>
--- registry.php
+++ registry.php
@@ -11,7 +11,7 @@
     <div class="registryArea__inner">
         <div class="registryArea__title">Регистрация</div>
         <div class="registryArea__form">
-            <form method="post">текст
+            <form method="post">
                 <input type="email" name="login" required placeholder="Почта" value="<?php echo $_POST['login']; ?>">
                 <input type="text" name="name" pattern="^[A-Za-zА-Яа-я]+$" required placeholder="Имя" value="<?php echo $_POST['name']; ?>">
                 <input type="text" name="surname" pattern="^[A-Za-zА-Яа-я]+$" placeholder="Фамилия" value="<?php echo $_POST['surname']; ?>">
@@ -32,41 +32,35 @@
             </form>
             <?php
             require_once 'data/php/connection.php';
+            require_once 'data/php/reg/except.php';
             $link = new mysqli($sqli_host, $sqli_user, $sqli_password, $sqli_database) or die("Ошибка " . mysqli_error($link));
             $dataBase = $link->query("SELECT COUNT(*) as exist FROM `user` WHERE `login` = '" . $_POST['login'] . "' OR `phone` = '" .$_POST['phone']. "'");
             $dataArr = $dataBase->fetch_assoc();
+
             if (isset($_POST['registryButton'])) {
-                if(preg_match("/^[a-zA-Z0-9]{5,}@[a-zA-Z]{3,}.[a-zA-Z]{2,}$/", $_POST['email'])) {
 
-                }
-                if(preg_match("/^\S+/", $_POST['name'])) {
+                $registryCheck = new RegistryExc();
+                $registryCheck->checkEmail($_POST['login'], "Неверно введена почта.");
+                $registryCheck->checkName($_POST['name'], "В имени разрешено использовать только русские, латинские буквы и цифры.");
+                $registryCheck->checkPhone($_POST['phone'], "Неверно введден номер телефона.");
+                $registryCheck->checkAge($_POST['age'], "Неверно указан воз");
+                $registryCheck->checkPassword($_POST['password'], $_POST['passwordCheck'], "Пароль должен состоять как минимум из 8 имволов.", "В пароле использованы недопустимые символы.", "Пароли не совпадают.");
+                $registryCheck->checkExistence($dataArr['exist'], "Данные ваших реквизитов уже зарегистрированы.");
 
-                }
-                if(preg_match("/^[1-9][0-9]{10}$|^[1-9]\s[0-9]{3}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}$|^[1-9]-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$/", $_POST['phone'])) {
-
-                }
-                if(preg_match("/^[0-9]{1,3}$/", $_POST['age'])) {
-
-                }
-
-                if ($dataArr['exist']) {
-                    echo "<div class='error'>Данные ваших реквизитов уже зарегистрированы.</div>";
-                }
-                else if ($_POST['password'] != $_POST['passwordCheck']) {
-                    echo "<div class='error'>Введенные пароли не совпадают.</div>";
-                }
-                else if ($link->query("INSERT INTO `user`(`login`, `password`, `phone`, `name`, `surname`, `age`)
-                        VALUES ('" . $_POST['login'] . "', '" . hash('md5', $_POST['password'], false) . "', '" . $_POST['phone'] . "', '" . $_POST['name'] . "', '" . $_POST['surname'] . "',  " . $_POST['age'] . ")")) {
-                    session_start();
-                    $_SESSION["login"] = $_POST['login'];
-                    header('Location: userPage.php');
-                }
-                else {
-                    echo "<div class='error'>Не удалось зарегистрироваться.</div>";
+                if (!$registryCheck->getExceptions()) {
+                    if ($link->query("INSERT INTO user(login, password, phone, name, surname, age, id_status)
+                            VALUES ('" . $_POST['login'] . "', '" . hash('md5', $_POST['password'], false) . "', '" . $_POST['phone'] . "', '" . $_POST['name'] . "', '" . $_POST['surname'] . "',  " . $_POST['age'] . ", '1')")) {
+                        session_start();
+                        $_SESSION["login"] = $_POST['login'];
+                        header('Location: userPage.php');
+                    }
+                    else {
+                        echo "<div class='error'>Не удалось зарегистрироваться.</div>";
+                    }
                 }
             }
             ?>
-        <p><a class="personalArea__register" href="index.php">Уже зарегистрированы?</a></p>
+        <p><a class="personalArea__register" href="authorization.php">Уже зарегистрированы?</a></p>
         </div>
     </div>
 </div>
